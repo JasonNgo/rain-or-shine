cb6bc1efbd21680dafd0ebce7354415387a45e9a
Jason Ngo <jasonnngo@gmail.com>
2016-10-19 19:15:05 -0400

    Initial commit

---
+++ rain-or-shine/WeatherCell.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherCell.swift
+//  rain-or-shine
+//
+//  Created by Jason Ngo on 2016-10-19.
+//  Copyright © 2016 Jason Ngo. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
---
+++ rain-or-shine/WeatherVC.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  rain-or-shine
+//
+//  Created by Jason Ngo on 2016-10-19.
+//  Copyright © 2016 Jason Ngo. All rights reserved.
+//
+
+import UIKit
+
+let _KEY = "ddb7d906b1618250b1311e20360f6fa8"
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var currentTempLbl: UILabel!
+    @IBOutlet weak var locationLbl: UILabel!
+    @IBOutlet weak var currentWeatherImg: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath)
+        
+        return cell
+    }
+}
+
